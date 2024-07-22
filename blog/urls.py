385f1d30d1e28dc7b10d9e8d7dad88a075de74bb
Tejaswi92404 <gothula.tejaswi@gmail.com>
2024-07-22 18:30:36 +0530

    To create blog using Django framework
    To create blog using Django framework

---
+++ blog/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import PostListCreate, PostDetail, CommentListCreate
+
+urlpatterns = [
+    path('posts/', PostListCreate.as_view(), name='post-list-create'),
+    path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    path('posts/<int:post_pk>/comments/', CommentListCreate.as_view(), name='comment-list-create'),
+]
+"""
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns += [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+"""
---
+++ blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+class PostListCreate(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class CommentListCreate(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return Comment.objects.filter(post_id=self.kwargs['post_pk'])
+
+    def perform_create(self, serializer):
+        post = Post.objects.get(pk=self.kwargs['post_pk'])
+        serializer.save(author=self.request.user, post=post)
