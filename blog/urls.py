34d9774c77c6327ec0b6fb44ba8a460705c9e704
Tejaswi92404 <gothula.tejaswi@gmail.com>
2024-09-21 17:49:53 +0530

    dev branch changes

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import PostListCreate, PostDetail, CommentListCreate,GetDataCustom
 
 urlpatterns = [
+    #branch changes
     path('posts/', PostListCreate.as_view(), name='post-list-create'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
     path('posts/<int:post_pk>/comments/', CommentListCreate.as_view(), name='comment-list-create'),
